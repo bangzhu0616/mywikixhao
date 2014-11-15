7e5f1b706cd522d7d21611b8beb00330aff8b938
xiaofeng <xhao1985@gmail.com>
2014-11-14 20:45:37 -0700

    first commit. finish login,signup,frontpage.

---
+++ main.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import jinja2
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+										autoescape = True)
+
+from google.appengine.ext import db
+
+import signup
+
+loginflag = 0
+
+class WikiHandler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template,**kw))
+
+class Pages(db.Model):
+	id = db.IntegerProperty(required = True)
+	pagename = db.StringProperty()
+	content = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	last_modified = db.DateTimeProperty(auto_now = True)
+
+class User(db.Model):
+	username = db.StringProperty(required = True)
+	password = db.StringProperty(required = True)
+	email = db.StringProperty()
+	created = db.DateTimeProperty(auto_now_add = True)
+
+class Signup(WikiHandler):
+	def get(self):
+		self.render("signup.html")
+
+	def post(self):
+		have_error = False
+		username = self.request.get('username')
+		password = self.request.get('password')
+		verify = self.request.get('verify')
+		email = self.request.get('email')
+
+		params = dict(username=username, email=email)
+
+		if not signup.verify_username(username):
+			params['error_username'] = "That's not a valid username."
+			haver_error = True
+
+		if not signup.verify_pwd(password):
+			params['error_verify'] = "That's not a valid password."
+			have_error = True
+		elif not signup.match_pwd(password, verify):
+			params['error_verify'] = "Your password do not match."
+			have_error = True
+
+		if not signup.verify_em(email):
+			params['error_email'] = "That's not a valid email."
+			have_error = True
+
+		if have_error:
+			self.render('signup.html', **params)
+		else:
+			user = User.all().filter('username =', username).get()
+			if user:
+				self.render('signup.html', error_username="That user already exists.")
+			else:
+				newuser = User(username=username,
+								password=signup.make_pw_salt(username,password),
+								email=email)
+				newuser.put()
+				global loginflag
+				loginflag = 1
+				self.redirect('/')
+
+class Login(WikiHandler):
+	def get(self):
+		self.render("login.html")
+
+	def post(self):
+		username = self.request.get('username')
+		password = self.request.get('password')
+		error = 'Invalid login!'
+		user = User.all().filter('username =', username).get()
+		if not user:
+			self.render('login.html',error_login=error)
+		else:
+			user_hash = user.password
+			if signup.check_pwd(username, password, user_hash):
+				global loginflag
+				loginflag = 1
+				self.redirect('/')
+			else:
+				self.render('login.html',error_login=error)
+
+class FrontPage(WikiHandler):
+	def get(self):
+		global loginflag
+		fp = db.GqlQuery("select * from Pages where id = 1")
+		self.render('wikipage.html', login=loginflag, 
+									pagename='',
+									username='',
+									pagecontent=fp)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', FrontPage),
+    ('/signup', Signup),
+    ('/login', Login)
+], debug=True)
---
+++ signup.py
@@ -0,0 +1,61 @@
+import re
+import hashlib
+import hmac
+import random
+import string
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PWD_RE = re.compile(r"^.{3,20}$")
+EM_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+
+secret = ":m2#[Bc+Mb>Yw[|xhaowiki"
+
+def verify_username(username):
+	return USER_RE.match(username)
+
+def verify_pwd(pwd):
+	return PWD_RE.match(pwd)
+
+def match_pwd(pwd, ver):
+	if pwd==ver:
+		return True
+	else:
+		return False
+
+def verify_em(em):
+	if em=='':
+		return True
+	return EM_RE.match(em)
+
+def make_salt():
+	return ''.join([random.choice(string.letters) for i in range(5)])
+
+def make_pw_salt(name, pw):
+	salt = make_salt()
+	return hashlib.sha256(name+pw+salt).hexdigest()+','+salt
+
+def check_pwd(name, pw, ha):
+	a = string.split(ha, ',')
+	hnp = a[0]
+	salt = a[1]
+	if hashlib.sha256(name+pw+salt).hexdigest()==hnp:
+		return True
+	else:
+		return False
+
+def make_secure_val(s):
+	return "%s|%s" % (s, hash_pwd(s))
+
+def check_secure_val(h):
+	val = h.split('|')[0]
+	if h == make_secure_val(val):
+		return val
+
+def set_secure_cookie(self, name, val):
+	cookie_val = make_secure_val(val)
+	self.response.headers.add_header('Set-Cookie',
+								'%s=%s; Path=/' % (str(name), str(cookie_val)))
+
+def read_secure_cookie(self, name):
+	cookie_val = self.request.cookies.get(name)
+	return cookie_val and check_secure_val(cookie_val)
